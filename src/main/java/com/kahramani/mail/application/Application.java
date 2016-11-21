2458181cbe9c8abe895ba2bb28daab894d6667b0
kahramani <polapierre@gmail.com>
2016-11-21 16:01:41 +0300

    application main class added

---
+++ src/main/java/com/kahramani/mail/application/Application.java
@@ -0,0 +1,10 @@
+package com.kahramani.mail.application;
+
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by kahramani on 11/21/2016.
+ */
+@ComponentScan("com.kahramani.mail.*")
+public class Application {
+}
